5ffbd6551f5b9ee21bb41827fbe6bc6ae52f3622
Trigun Joshi <trigunjoshi@gmail.com>
2022-03-23 18:34:02 -0700

    about page setup

---
+++ my-blog/src/pages/AboutPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <>
+      <h1>AboutPage</h1>
+      <p>Lorem ipsum dolor sit amet, gubergren consetetur cu vel, mel ex audiam percipit legendos. Cu maiorum adversarium quo. Vix erant simul sapientem ut. Paulo tempor ponderum no mei, rebum doming tritani an vix, nec commune deseruisse percipitur no. Ex omnis error utroque his, offendit voluptua deseruisse et per. Zril choro petentium an est.</p>
+    </>
+  )
+}
+
+export default AboutPage
